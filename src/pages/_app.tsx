8ec89812764b1d39ebc7c0e189bead94aae2a1d9
Gustavo Galupo <ggalupo2@hotmail.com>
2022-11-17 18:47:09 -0300

    Create global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from "next/app";
 
+import { globalStyles } from "../styles";
+
 export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
+
   return <Component {...pageProps} />;
 }
