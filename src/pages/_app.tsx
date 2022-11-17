0000553d14d8d3f51cc95c102189488ebb02ddc9
Gustavo Galupo <ggalupo2@hotmail.com>
2022-11-17 19:02:04 -0300

    Create Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
 import type { AppProps } from "next/app";
+import Image from "next/image";
 
 import { globalStyles } from "../styles";
+import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image
+          src="/logo-ignite-shop.svg"
+          alt="Ignite Shop logo"
+          width={130}
+          height={52}
+        />
+      </Header>
+      <Component {...pageProps} />;
+    </Container>
+  );
 }
