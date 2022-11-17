7647d2dc5c000df8c7d1d9f36e3435811c32302e
Gustavo Galupo <ggalupo2@hotmail.com>
2022-11-17 18:50:38 -0300

    Add colors to theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import type { AppProps } from "next/app";
 
 import { globalStyles } from "../styles";
 
-export default function App({ Component, pageProps }: AppProps) {
-  globalStyles();
+globalStyles();
 
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
