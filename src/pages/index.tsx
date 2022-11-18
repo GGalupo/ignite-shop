e119fb28c4ab9457f2aa6ca22c37fd2a3ebcd8c5
Gustavo Galupo <ggalupo2@hotmail.com>
2022-11-18 20:08:11 -0300

    Add carousel to product list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 import shirt1 from "../../public/shirts/shirt-1.png";
 import shirt2 from "../../public/shirts/shirt-2.png";
@@ -6,9 +8,16 @@ import shirt3 from "../../public/shirts/shirt-3.png";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.5,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={480} alt="Shirt 1" />
 
         <footer>
@@ -17,11 +26,29 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} width={520} height={480} alt="Shirt 2" />
 
         <footer>
           <strong>Shirt number 2</strong>
+          <span>$39.90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="Shirt 3" />
+
+        <footer>
+          <strong>Shirt number 3</strong>
+          <span>$39.90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="Shirt 3" />
+
+        <footer>
+          <strong>Shirt number 3</strong>
           <span>$39.90</span>
         </footer>
       </Product>
