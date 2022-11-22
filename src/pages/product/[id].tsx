e0745f057b77f90eabc27aadd4d62887acccf53c
Gustavo Galupo <ggalupo2@hotmail.com>
2022-11-22 20:41:41 -0300

    Product page styles

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,34 @@
 import { type NextPage } from "next";
+// import Image from "next/image";
 import { useRouter } from "next/router";
+
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 const Product: NextPage = () => {
   const { query } = useRouter();
 
-  return <h1>Product {query.id} page</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image /> */}</ImageContainer>
+
+      <ProductDetails>
+        <h1>X Shirt</h1>
+        <span>$19.90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
+          asperiores. Ad rerum vitae ut nam nulla error saepe voluptatibus nisi
+          quidem earum blanditiis commodi itaque labore officia sit, ex beatae.
+        </p>
+
+        <button>Buy now</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 };
 
 export default Product;
